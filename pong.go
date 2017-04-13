83a57941fc87305a2e7d8705c56e9f82b680ffa0
benjamingrogg <benjamin.grogg@thetube.ch>
2017-04-13 09:28:04 +0200

    Add Hostname

--- pong.go
+++ pong.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+	"fmt"
+)
 
 func main() {
+
+
+	name, _ := os.Hostname()
+	fmt.Println("Hostname : %s\n", name)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "pong", "hostname": name,
 		})
 	})
 	r.Run(":8888") 
